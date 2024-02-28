28f146d042e5a94998b2197adabdc1c0b142b061
Ali Abdelrahman <117834271+Alielsalek1@users.noreply.github.com>
2024-02-28 03:22:03 +0200

    initiated the crawling

--- src/crawlManager.js
+++ src/crawlManager.js
@@ -1,22 +1,40 @@
+const JSDOM = require("jsdom");
+const fetch = require("node-fetch");
+
 class CrawlManager {
     constructor(website) {
-        this.pages = new Map();
+        this.title = "";
         this.root = website;
-        this.TotalWordCount = 0;
-        this.TotalImgCount = 0;
-        this.TotalLinks = 0;
-        this.Title = website.split['.'][0];
+        this.totalWordCount = 0;
+        this.totalImgCount = 0;
+        this.totalLinks = 0;
+        this.totalCrawlTime = 0;
         this.internalLinks = [];
         this.externalLinks = [];
         this.brokenLinks = [];
+        this.visited = new Map();
         this.pageCrawlTime = new Map();
         this.pageDepth = new Map();
-        this.totalCrawlTime = 0;
     }
-    crawl() {
+    async crawl() {
+        console.log(`Starting Business!`);
+
+        //requesting the HTML from the page
+        const resp = await fetch(root);
+        const htmlText = await resp.text();
         
+        //setting the page title with dom
+        const dom = new JSDOM(htmlText);
+        this.title = dom.title;
+
+        //crawling!!!
+        this.crawlPage(root);
     }
-    crawlPage() {
+    async crawlPage(page) {
+        console.log(`Actively crawling ${page}`);
+
+        //setting the visited to true to not infinetly crawl
+        this.visited.set(root, true);
 
     }
     countWords() {
