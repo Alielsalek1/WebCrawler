1a7a91372ff330f4ca173067c583642db5ef54c4
Ali Abdelrahman <117834271+Alielsalek1@users.noreply.github.com>
2024-03-06 01:33:12 +0200

    files ready to be exported, but there needs to be a newer version that does not await every call in crawlPage

--- src/crawlManager.js
+++ src/crawlManager.js
@@ -3,24 +3,21 @@ const puppeter = require('puppeteer');
 
 class CrawlManager {
     constructor(website) {
-        this.maxDepth = 3;
+        this.maxDepth = 1;
         this.title = "";
         this.website = website.root;
         this.totalWordCount = 0;
         this.totalImgCount = 0;
         this.totalLinks = 0;
         this.totalCrawlTime = 0;
-        this.externalLinks = [];
         this.linkCnt = new Map();
         this.pageCrawlTime = new Map();
+        this.browser = null;
     }
     async getHtml(link) {
         try {
-            //launching the browser
-            const browser = await puppeter.launch();
-    
             //generating a new tab
-            const page = await browser.newPage();
+            const page = await this.browser.newPage();
     
             //going to the website
             await page.goto(link);
@@ -34,41 +31,49 @@ class CrawlManager {
             //get the HTML
             const htmlContent = await page.content();
     
-            //close the browser to free up resources
-            await browser.close();
+            //close the page to free up resources
+            await page.close();
     
             return htmlContent;
         }
         catch(err) {
             console.log(`Error: ${err}`);
-            return undefined;
+        }
+    }
+    async sumWords(dom) {
+        /*getting all HTMl tags as each element is a single instance.
+        EX: if we have 3 <p> then the 3 instances of P will be included each as an element */
+        const allElements = dom.window.document.querySelectorAll("*");
+        for (const element of allElements) {
+            const text = element.textContent; //Get text content
+            const words = text.split(/\s+/); //Split into words using regex for whitespace
+            this.totalWordCount += words.length; //Count words
         }
     }
     async crawl() {
         console.log(`Starting Business!!`);
 
+        //launching the browser
+        this.browser = await puppeter.launch();
+
         //crawling!!!
-        this.crawlPage(this.website);
+        await this.crawlPage(this.website);
     }
     async crawlPage(page, depth = 0) {
-        if (depth > this.maxDepth) return;
-
-        console.log(`Actively crawling ${page}`);
-
-        //assigning the start date to calculate the crawl time for each page
-        const startTime = Date.now();
-
         //check if the current page is visited
-        if (this.linkCnt.get(page) > 0) {
+        if (this.linkCnt.has(page)) {
             // Increment the Link count by 1
             let currCnt = this.linkCnt.get(page);
             this.linkCnt.set(currCnt + 1);
             return;
         }
 
+        //assigning the start date to calculate the crawl time for each page
+        const startTime = Date.now();
+        console.log(`Actively crawling ${page} with depth ${depth}`);
+
         //setting the link counter to 1 to prevent infinite Crawl
         this.linkCnt.set(page, 1);
-
         //incrementing totalLinks
         this.totalLinks++;
 
@@ -76,57 +81,52 @@ class CrawlManager {
             //getting the page HTML
             const htmlText = await this.getHtml(page);
 
+            if (!htmlText) 
+                throw new Error("no HTML retrieved");
+
             //declaring the DOM
             const dom = new JSDOM(htmlText);
 
             //getting the page's title
             if (depth === 0)
-                this.title = dom.title; 
+                this.title = dom.window.document.title; 
 
             //adding to the variables computing total attributes in the Website
             this.totalImgCount += dom.window.document.querySelectorAll('img').length;
-            
-            /*getting all HTMl tags as each element is a single instance.
-              EX: if we have 3 <p> then the 3 instances of P will be included each as an element */
-            const allElements = dom.window.document.querySelectorAll("*");
-            for (const element of allElements) {
-                const text = element.textContent; //Get text content
-                const words = text.split(/\s+/); //Split into words using regex for whitespace
-                this.totalWordCount += words.length; //Count words
-            }
+
+            console.log(`updating values`)
+            let crawlTime = Date.now() - startTime; //page CrawlTime
+            this.totalCrawlTime += crawlTime; //adding the page crawlTime to the totalCrawlTime
+            this.pageCrawlTime.set(page, crawlTime) //mapping the page to its crawlTime
+            // adding all the words in the page to the counter
+            await this.sumWords(dom)
 
             //getting the all links from the DOM
             let links = dom.window.document.querySelectorAll('a');
 
-            //looping to crawl each page if it is internal
-            links.forEach(link => {
-                if (link.getAttribute('href')) {
-                    let l1 = new URL(link.getAttribute('href')), l2 = new URL(this.website);
+            //return if you are at max depth
+            if (depth == this.maxDepth) return;
 
+            //looping to crawl each page if it is internal
+            for (let link of links) {
+                //get the new path
+                const href = link.getAttribute('href');
+
+                if (href) {
+                    //concatonate the path with the original website
+                    let l1 = new URL(this.website + href.slice(1));
+                    //convert the base website to a URL to make it comparable with l1
+                    let l2 = new URL(this.website);
+                    
                     // if the the current URL has the same hostname as the root hostname then they are on the same page
-                    if (l1.hostname === l2.hostname) {
-                        this.crawlPage(l1, depth + 1);
-                    }
-                    else
-                        this.externalLinks.push(link.getAttribute('href')); //append the link to external links array
+                    if (l1.hostname === l2.hostname)
+                       await this.crawlPage(l1.href, depth + 1);
                 }  
-            });
+            }
         }
         catch (err) {
-            console.log(`Error: ${err}`);
+            console.log(`Error: ${err} with page ${page}`);
         }
-
-        let crawlTime = Date.now() - startTime; //page CrawlTime
-        this.totalCrawlTime += crawlTime; //adding the page crawlTime to the totalCrawlTime
-        this.pageCrawlTime.set(page, crawlTime) //mapping the page to its crawlTime
-
-        //temp testing
-        console.log(this.totalCrawlTime)
-        console.log(this.totalImgCount)
-        console.log(this.totalLinks)
-        console.log(this.totalWordCount)
-        console.log(this.externalLinks)
-        console.log("_________________")
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const CrawlManager = require('./crawlManager.js');
 async function main() {
     console.log("App Start");
 
-    const site = new Website('https://mohamed-amr7.github.io/Movie-Search-App/');
+    const site = new Website('https://blog.boot.dev/');
     const crawler = new CrawlManager(site);
     await crawler.crawl();
+
+    console.log(`---------------------------------------`)
+    console.log(crawler.linkCnt);
+    console.log(crawler.pageCrawlTime);
+    console.log(crawler.title);
+    console.log(crawler.totalCrawlTime);
+    console.log(crawler.totalImgCount);
+    console.log(crawler.totalLinks);
+    console.log(crawler.totalWordCount);
 }
 
 main();
